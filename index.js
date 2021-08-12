af72727423e6656e1f9470256a2c5523a20bd298
William Mosto <will.mosto@gmail.com>
2021-08-12 16:43:51 -0400

    commit tree 41870bc15e2a92552f27b7a47d71898d5eb72f9a

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = [4, 5, 3, 4, 4, 6, 5].reduce(function(accumulator, currentValue){ return accumulator + currentValue})
+
+
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+// const totalBatteries = (accumulator, currentValue) => parseInt(accumulator + currentValue)
+// console.log(batteryBatches.reduce(totalBatteries))
+// => 31 
